8cc38ab45f7f5a16043ebcdfb1ba16b2ff78da84
Miguel Ramos <miguel.ramos@inventsoftware.com.br>
2024-10-06 00:25:42 -0300

    Integrando com zap

--- TesteGpt.py
+++ TesteGpt.py
@@ -1,9 +1,38 @@
 import os
+from flask import Flask, request
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
 import google.generativeai as genai
 
 key = os.environ['GEMINI_API_KEY']
 
 genai.configure(api_key=key)
+
+app = Flask(__name__)
+
+twilio_account_sid = ''
+twilio_auth_token = ''
+twilio_whatsapp_number = ''
+
+client = Client(twilio_account_sid, twilio_auth_token)
+
+@app.route('/whatsapp', methods=['POST'])
+def whatsapp_reply():
+    incoming_msg = request.form.get('Body')
+    sender_number = request.form.get('From')
+
+    response = MessagingResponse()
+    response.message(f'Sua mensagem foi recebida: {incoming_msg}')
+
+    resposta_ia = consultar_ia(incoming_msg)
+
+    client.messages.create(
+        from_=f'whatsapp:{twilio_whatsapp_number}',
+        body=resposta_ia,
+        to=sender_number
+    )
+
+    return str(response)
 
 def consultar_ia(pergunta):
     model = genai.GenerativeModel('gemini-1.5-flash')
@@ -11,7 +40,5 @@ def consultar_ia(pergunta):
 
     return response.text
 
-if __name__ == "__main__":
-    pergunta = input("Digite sua mensagem para o Gemini: ")
-    resposta = consultar_ia(pergunta)
-    print(f"Gemini: {resposta}")
+if __name__ == '__main__':
+    app.run(port=3000)
